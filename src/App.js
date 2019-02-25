9290d5250bf491c4761333532038d01402b52f65
prad0004 <prad0004>
2019-02-25 10:46:29 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import {createLogger} from 'redux-logger';
+import thunk from 'redux-thunk'
+import reducer from './Reducer.js';
+import { BrowserRouter as Router } from "react-router-dom";
+import HomeScreen from './screens/HomeScreen.js';
+
+const middleware = [thunk];
+if (process.env.NODE_ENV !== 'production'){
+  middleware.push(createLogger());
+}
+
+const store = createStore(reducer, applyMiddleware(...middleware));
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider store={store}>
+        <Router>  
+          <HomeScreen />
+        </Router>
+      </Provider>
     );
   }
 }
