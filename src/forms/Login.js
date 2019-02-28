5e8d7c1042a6de5ea414efe329e5f31cb788d1c4
Dimitri Dumont <hylwebmaster@gmail.com>
2019-02-28 19:32:01 +0100

    Ajout de l'App Bar sur le formulaire de connexion

--- src/forms/Login.js
+++ src/forms/Login.js
@@ -3,6 +3,9 @@ import '../assets/css/FormLogin.css';
 import {connect} from "react-redux";
 import {requestLogin, requestLogout} from "../Actions";
 import {Link} from "react-router-dom";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 class Login extends Component {
     componentDidMount() {
@@ -12,6 +15,14 @@ class Login extends Component {
     render() {
         return (
             <>
+                <AppBar position="static" color="primary">
+                    <Toolbar>
+                        <Typography variant="h6" color="inherit">
+                            App Cadeaux
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+
                 <form className="formLogin" onSubmit={(e) => {
                     e.preventDefault();
                     this.props.dispatch(requestLogin(e.target.elements[0].value, e.target.elements[1].value));
